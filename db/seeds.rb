bce1ea24e924c1682a13e99c3ad7591113a08ddc
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-10-13 15:47:02 -0500

    started adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Equipment.destroy_all 
+LoadStatus.destroy_all 
+TruckStatus.destroy_all
+
+puts 'seeding equipment'
+van = Equipment.create(equipment_type: "Van")
+reefer = Equipment.create(equipment_type: "Reefer")
+vr = Equipment.create(equipment_type: "Van / Reefer")
+flatbed = Equipment.create(equipment_type: "Flatbed")
+conestoga = Equipment.create(equipment_type: "Conestoga")
+stepdeck = Equipment.create(equipment_type: "Step deck")
+puts 'finished seeding equipment'
+
+puts 'seeding load statuses'
+opn = LoadStatus.create(status: "Open")
+booked = LoadStatus.create(status: "Booked")
+complete = LoadStatus.create(status: "Complete")
+puts 'finished seeding load statuses'
+
+puts 'seeding truck statuses'
+unassigned = TruckStatus.create(status: "Unassigned")
+assigned = TruckStatus.create(status: "Assigned")
+dispatched = TruckStatus.create(status: "Dispatched")
+atpickup = TruckStatus.create(status: "At pickup")
+loaded = TruckStatus.create(status: "loaded")
+atdelivery = TruckStatus.create(status: "At delivery")
+delivered = TruckStatus.create(status: "Delivered")
+puts 'finished seeding truck statuses'
+
+puts 'seeding sites'
+Site.create(name: "Steel of West Virginia, Inc.", city: "Huntington", state: "WV", address: "17th Street &, 2nd Ave", zip: 25703, contact_name: "John Newman", contact_email: "John@fake.com", contact_phone: "304-696-8200")
+puts 'finished seeding sites'
+
+puts 'seeding fees_types'
+    linehaul = FeeType.create(name: "Linehaul")
+    detention = FeeType.create(name: "Detention")
+    extrastop = FeeType.create(name: "Extra Stop")
+    lumper = FeeType.create(name: "Lumper")
+puts 'finished seeding fees_types'
