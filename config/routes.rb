feef67169d61416c5f736b8d64a472a111329a82
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-10-13 18:30:40 -0500

    added backend auth

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   resources :carriers
   resources :customers
   resources :loads
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
