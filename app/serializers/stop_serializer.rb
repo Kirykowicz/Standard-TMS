44b3b419a6439902d8169d721c01ec5efb74da02
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-10-23 12:00:35 -0500

    succesfully got the origin to render onto the map

--- app/serializers/stop_serializer.rb
+++ app/serializers/stop_serializer.rb
@@ -1,8 +1,12 @@
 class StopSerializer < ActiveModel::Serializer
-  attributes :id, :load_id, :site_id, :date, :time, :notes, :site_name, :site_city, :site_state
+  attributes :id, :load_id, :site_id, :date, :time, :notes, :site_name, :site_address, :site_city, :site_state, :site_zip
 
   def site_name
     object.site.name
+  end
+
+  def site_address
+    object.site.address
   end
 
   def site_city
@@ -11,5 +15,9 @@ class StopSerializer < ActiveModel::Serializer
 
   def site_state
     object.site.state
+  end
+
+  def site_zip
+    object.site.zip
   end
 end
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 function Map({ individualLoad }) {
-  const origin = individualLoad.stops[0];
+  const [originLatLng, setOriginLatLng] = useState({});
+
+  const originAddress = individualLoad.stops[0].site_address.split("");
+  const originCity = individualLoad.stops[0].site_city.split(" ");
+  for (let i = 0; i < originAddress.length; i++) {
+    if (originAddress[i] === " ") {
+      originAddress[i] = "%20";
+    }
+  }
+  for (let i = 0; i < originCity.length; i++) {
+    if (originCity[i] === " ") {
+      originAddress[i] = "%20";
+    }
+  }
+
   const destination = individualLoad.stops[0];
+  const origin = `${originAddress.join("")}%20${originCity.join("")}%20${
+    individualLoad.stops[0].site_state
+  }%20${individualLoad.stops[0].site_zip}`;
 
   const containerStyle = {
     width: "100vw",
@@ -15,16 +33,22 @@ function Map({ individualLoad }) {
     lng: -87.6298,
   };
 
-  //   fetch(
-  //     `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyC54Xv04BjwdUTZfIMI897dVZRVlCtgue0`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((res) => console.log(res.results[0].geometry.location));
+  useEffect(() => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${origin}&key=AIzaSyC54Xv04BjwdUTZfIMI897dVZRVlCtgue0`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setOriginLatLng(res.results[0].geometry.location);
+        console.log(res.results[0].geometry.location);
+      });
+  }, []);
 
   return (
     <LoadScript googleMapsApiKey="AIzaSyC54Xv04BjwdUTZfIMI897dVZRVlCtgue0">
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
         <Marker key="1" position={{ lat: 41.8781, lng: -87.6298 }} />
+        <Marker key="2" position={originLatLng} />
       </GoogleMap>
     </LoadScript>
   );
